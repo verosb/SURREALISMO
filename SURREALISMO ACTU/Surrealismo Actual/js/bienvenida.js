597d0c8cda0a1f5772c905f94c16d3b64f4bf2a1
Veronica <vbsanchezb@ufpso.edu.co>
2023-06-26 12:38:23 -0500

    Agregando archivos pagina

---
+++ SURREALISMO ACTU/Surrealismo Actual/js/bienvenida.js
@@ -0,0 +1,3 @@
+function bienvenida(){
+    alert("Bienvenido al mundo del Surrealismo y el arte... Gracias por visitarnos");
+}
---
+++ SURREALISMO ACTU/Surrealismo Actual/js/colores.js
@@ -0,0 +1,11 @@
+function color1(){
+    document.getElementById('body').style.backgroundColor="#59c743";
+    document.getElementById('color1')
+}
+function color2(){
+    document.getElementById('body').style.backgroundColor="#b14fd8";
+}
+
+function color3(){
+    document.getElementById('body').style.backgroundColor="#f588ac";
+}
